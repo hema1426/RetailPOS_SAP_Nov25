013b6f4fa1df0111cb396261a5b02e4d4e46dcad
hema1414 <hemaalpsdeal@gamil.com>
2025-11-26 10:12:31 +0530

    url updating and color change in button

--- app/src/main/java/com/winapp/retailpos_sap/ui/utils/Constants.java
+++ app/src/main/java/com/winapp/retailpos_sap/ui/utils/Constants.java
@@ -29,7 +29,7 @@ public class Constants extends BaseApp {
 
 //  public static String BASEURL = "http://136.243.60.223:8090/api/";
 
-    public static String BASEURL = "https://c21326-easysales-onlytesting.cloudiax.com/api/";
+    public static String BASEURL = "http://172.16.5.72:91/api/";
 
   //  TRANS_ORIENT_DEMO ="https://c21326-easysales-onlytesting.cloudiax.com/api/";
    // WMS_KH_CYCLE_DELIVERY_DEMO ="http://136.243.60.223:8090/api/";
